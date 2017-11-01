e62fe19309a26f5b258b48ecedb656eb3f11ef0e
Gary <gear@thytruth.com>
2017-11-01 02:21:07 -0700

    added missing file

---
+++ install_geolocat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /etc/nginx/geoip
+cd /etc/nginx/geoip
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+gunzip GeoIP.dat.gz
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+gunzip GeoLiteCity.dat.gz
